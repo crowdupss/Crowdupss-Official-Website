a40b389f1b58a24e23a5ec9d997a4d9f687e6191
Shri Srivastava <76124911+shri33@users.noreply.github.com>
2024-01-04 01:17:54 +0530

    Add files via upload

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 // Navbar.js
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSignUpClick = () => {
+    // Navigate to the signup page when the "Sign Up" button is clicked
+    navigate('/signup');
+  };
 
   return (
     <nav className="bg-black dark:bg-black fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
@@ -21,6 +27,7 @@ const Navbar = () => {
         <div className="flex md:order-3 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
+            onClick={handleSignUpClick}  // Call the handleSignUpClick function
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Sign up
@@ -49,59 +56,7 @@ const Navbar = () => {
             navbarOpen ? 'block' : 'hidden'
           } md:hidden items-center justify-between w-full md:flex md:w-auto md:order-1`}
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/"
-                className="block py-3 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="relative group">
-              <Link
-                to="/about"
-                className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About Us
-              </Link>
-              <ul className="absolute hidden bg-white dark:bg-gray-800 mt-2 space-y-2 rounded-md shadow-lg">
-                <li>
-                  <Link
-                    to="/about/our-story"
-                    className="block px-4 py-2 text-gray-700 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Our Story
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/about/team"
-                    className="block px-8 py-2 text-gray-700 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Team
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link
-                to="/events"
-                className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Events
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/resources"
-                className="block py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Resources
-              </Link>
-            </li>
-          </ul>
+          {/* ... (rest of the code) */}
         </div>
       </div>
     </nav>
